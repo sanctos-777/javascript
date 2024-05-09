107abe4d4bdb9a60771fd237829ffc307c731ad4
sanctos-777 <146378058+sanctos-777@users.noreply.github.com>
2024-05-09 17:18:08 -0300

    aula 21

---
+++ 009-array.js
@@ -0,0 +1,57 @@
+/*
+O que é um Array em JavaScript?
+Um array em JavaScript é um tipo de estrutura 
+de dados que permite armazenar múltiplos valores 
+em uma única variável. Os elementos de um array 
+são indexados numericamente, começando do índice 0,
+ o que significa que cada elemento tem uma posição 
+ específica no array.
+*/
+// Usando colchetes []
+let array1 = [1, 2, 3, 4, 5];
+
+// Usando o construtor Array()
+let array2 = new Array(1, 2, 3, 4, 5);
+
+// Criando um array vazio
+let array3 = [];
+
+// Arrays podem conter diferentes tipos de dados
+let array4 = ["Hello", 42, true];
+
+// Acessando indice do array
+let array = [10, 20, 30, 40, 50];
+console.log(array[0]); // Saída: 10
+console.log(array[2]); // Saída: 30
+
+// Verificando o tamanho do array
+let arraylength = [1, 2, 3];
+console.log(arraylength.length); // Saída: 3
+
+// inserindo um item no final do array
+arraylength.push(4);
+console.log(arraylength); // Saída: [1, 2, 3, 4]
+
+// removendo um item no final do array
+let removedElement = arraylength.pop();
+console.log(removedElement); // Saída: 4
+console.log(arraylength); // Saída: [1, 2, 3]
+
+var array5 = [1, 2, 3, 4, 5];
+
+// Índice do item a ser removido
+var indice = 2;
+
+// Remove o item na posição do índice
+array5.splice(indice, 3);
+
+console.log(array5); // Saída: [1, 2, 4, 5]
+
+var array6 = [1, 2, 4, 5];
+var index = 2; // Índice onde você deseja inserir o valor
+var newValue = 3; // Valor que você deseja inserir
+
+// Insere o novo valor na posição do índice
+array6.splice(index, 0, newValue);
+
+console.log(array6); // Saída: [1, 2, 3, 4, 5]
---
+++ 010-laço_de_repetição.js
@@ -0,0 +1,42 @@
+/*O que é o Laço for em JavaScript?
+O laço for é uma estrutura de controle 
+que permite executar repetidamente um bloco 
+de código um número específico de vezes.
+ É uma maneira conveniente de iterar sobre 
+ elementos em uma coleção, como um array,
+  ou executar uma ação repetida com base em uma 
+  condição. */
+
+  for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+const frutas = ['maça', 'banana', 'laranja', 'pera', 'melancia'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log('Eu gosto de ' + frutas[i]);
+}
+
+const pessoa = {
+    Nome: 'Rodrigo',
+    Sobrenome: 'Jesus',
+    Idade: 43
+  };
+  
+  // Este é um loop for...in que itera sobre todas as propriedades enumeráveis de um objeto chamado 'pessoa'.
+// 'pessoa' é o nome do objeto sobre o qual estamos iterando.
+
+for (let key in pessoa) {
+  // 'key' é uma variável que irá conter o nome de cada propriedade do objeto 'pessoa' durante cada iteração do loop.
+
+  // Este console.log() imprime o nome da propriedade atual (key) e seu valor correspondente (pessoa[key]) no console.
+  // 'key' é o nome da propriedade, enquanto 'pessoa[key]' acessa o valor da propriedade atual.
+  console.log(key + ': ' + pessoa[key]);
+}
+//o break serve para parar o laço de repetição
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      break;
+    }
+    console.log(i);
+  }
