b96115e62f3bbeee279d7fbbca4c352cd5cdb49f
sanctos-777 <146378058+sanctos-777@users.noreply.github.com>
2024-05-17 17:47:21 -0300

    17/05

---
+++ exercicio/exercicio5.js
@@ -0,0 +1,10 @@
+// Código: Declare duas variáveis, x e y, e atribua a elas os valores 10 e 20
+// respectivamente. Em seguida, troque os valores das variáveis
+
+function variaveis (x, y){
+        this.x=10
+        this.y=20
+}
+var variaveis1 = new variaveis(20, 10)
+
+console.log(variaveis1)
